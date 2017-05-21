72fc546f3b1ef24d4ace29408798c75fcc98ce2e
blindacai <linda_cai2014@hotmail.com>
2017-05-21 16:22:10 -0700

    create models of Question and Choice; set up db

--- polls/models.py
+++ polls/models.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
+import datetime
 
 # Create your models here.
+
+# two models, each represented by a class that subclasses models.Model
+# respective tables will be created in db
+
+@python_2_unicode_compatible
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- pollsite/settings.py
+++ pollsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',   # run makemigration on it
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
