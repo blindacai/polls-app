5fa8f22c8f8290f037c7c5f2350b66788bf8d1da
blindacai <linda_cai2014@hotmail.com>
2017-05-21 21:00:46 -0700

    use render() instead to display view

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
@@ -11,15 +10,18 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    # render returns an HttpResponse object of the given template rendered with the given context
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    details = "You're looking at question %s." % question_id
+    context = {'details': details}
+    return render(request, 'polls/detail.html', context)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
